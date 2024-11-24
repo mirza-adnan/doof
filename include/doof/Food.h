ebfa9da2cb90e5b4eaf13c49787e867282fa2462
Mirza Adnan <mirza.adnan2205@gmail.com>
2024-11-25 00:27:58 +0600

    add Food class

--- include/doof/Food.h
+++ include/doof/Food.h
@@ -10,23 +10,26 @@ private:
   float price;
   bool available;
   int id;
+  int restaurantId;
 
 public:
   Food();
 
-  Food(const string& _name, float _price, bool _available);
+  Food(const string& _name, float _price, bool _available, int _restaurantId);
 
   // getters
   string getName() const;
   float getPrice() const;
   bool isAvailable() const;
   int getId() const;
+  int getRestaurantId() const;
 
   // setters
   void setName(const string& _name);
   void setPrice(float _price);
   void setAvailable(bool _available);
   void setId(int _id);
+  void setRestaurantId(int _restaurantId);
 };
 
 #endif
--- src/Food.cpp
+++ src/Food.cpp
@@ -3,10 +3,11 @@
 // Default constructor
 Food::Food() {}
 
-Food::Food(const string& _name, float _price, bool _available) {
+Food::Food(const string& _name, float _price, bool _available, int _restaurantId) {
   name = _name;
   price = _price;
   available = _available;
+  restaurantId = _restaurantId;
 }
 
 // getters
@@ -26,6 +27,10 @@ int Food::getId() const {
   return id;
 }
 
+int Food::getRestaurantId() const {
+  return restaurantId;
+}
+
 // setters
 void Food::setName(const string& _name) {
   name = _name;
@@ -41,4 +46,8 @@ void Food::setAvailable(bool _available) {
 
 void Food::setId(int _id) {
   id = _id;
+}
+
+void Food::setRestaurantId(int _restaurantId) {
+  restaurantId = _restaurantId;
 }
