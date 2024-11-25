e1ececce2943900389c795f49c3c6928fb813112
Mirza Adnan <mirza.adnan2205@gmail.com>
2024-11-25 20:36:12 +0600

    add cin cout overload for RestaurantType and add AuthType

--- include/doof/enums.h
+++ include/doof/enums.h
@@ -1,6 +1,39 @@
-enum class RestaurantType {
-  FAST_FOOD,
-  CAFE,
-  FINE_DINING,
-  TOTAL
+#ifndef ENUMS_H
+#define ENUMS_H
+
+#include <iostream>
+using namespace std;
+
+enum RestaurantType {
+  RESTAURANT_TYPE_FAST_FOOD,
+  RESTAURANT_TYPE_CAFE,
+  RESTAURANT_TYPE_FINE_DINING,
+  RESTAURANT_TYPE_TOTAL
 };
+
+istream& operator >> (istream& is, RestaurantType& type) {
+  int a;
+  is >> a;
+  type = static_cast<RestaurantType>(a);
+
+  return is;
+}
+
+ostream& operator << (ostream& os, RestaurantType& type) {
+  if (type == RESTAURANT_TYPE_FAST_FOOD) {
+    os << "Fast Food";
+  }
+  else if (type == RESTAURANT_TYPE_CAFE) {
+    os << "Cafe";
+  }
+  else if (type == RESTAURANT_TYPE_FINE_DINING) {
+    os << "Fine Dining";
+  }
+}
+
+enum AuthType {
+  AUTH_TYPE_RESTAURANT,
+  AUTH_TYPE_CUSTOMER
+};
+
+#endif
