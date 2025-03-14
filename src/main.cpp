358d6a89409ecc79654d457686d7b6fda0031db5
Mirza Adnan <mirza.adnan2205@gmail.com>
2025-03-14 10:54:54 +0600

    enabled ansi escape codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <windows.h>
 #include <iostream>
 #include "doof/Restaurant.h"
 #include "doof/DB.h"
@@ -5,8 +6,21 @@
 #include "doof/Util.h"
 
 using namespace std;
+using std::byte;
+
+void enableAnsiEscapeCodes() {
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD dwMode = 0;
+  GetConsoleMode(hOut, &dwMode);
+  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(hOut, dwMode);
+}
 
 int main() {
+  enableAnsiEscapeCodes();
+
   App app;
   app.init();
+
+  return 0;
 }
